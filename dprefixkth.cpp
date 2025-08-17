f5f36fd04b0c35e2760060854c7739f12dac79bd
AhamedRobiul <mrobi19296798@gmail.com>
2025-08-17 23:44:42 +0600

    snd

--- dprefixkth.cpp
+++ dprefixkth.cpp
@@ -1,4 +1,4 @@
-#include<bits/stdc++.h>
+#include<bits/stdc++.h>//https://atcoder.jp/contests/abc234/tasks/abc234_d
 using namespace std;
 
 int main() {
@@ -8,8 +8,21 @@ int main() {
     
     int t;
     cin>>t;
-    while(t--){
-    
+    int k;cin>>k;
+    priority_queue<int,vector<int>,greater<int>>pq;
+    vector<int>v(t);
+    for(int i=0; i<t; i++){
+          cin>>v[i];
+    }
+    for(auto i:v){
+        pq.push(i);
+        if(pq.size()>k){
+            pq.pop();
+        }
+        if(pq.size()==k){
+            cout<<pq.top()<<endl;
+        }
+
     }
     return 0;
 }
